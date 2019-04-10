4cd18ea951a05200ab93e55893073bd5c481ff44
Julian Hatwell <julianhatwell@users.noreply.github.com>
2019-04-10 12:48:31 +0100

    hill

--- hill.R
+++ hill.R
@@ -11,20 +11,19 @@
 #       $REPORT - frequency of monitoring information
 #    type - "min" or "max"
 #    ... - extra parameters for FUN
-hclimbing=function(par,fn,change,lower,upper,control,
-                   type="min",...)
-{ fpar=fn(par,...)
-for(i in 1:control$maxit) 
-{ 
-  par1=change(par,lower,upper) 
-  fpar1=fn(par1,...)
-  if(control$REPORT>0 &&(i==1||i%%control$REPORT==0)) 
-    cat("i:",i,"s:",par,"f:",fpar,"s'",par1,"f:",fpar1,"\n")
-  if(   (type=="min" && fpar1<fpar) 
-        || (type=="max" && fpar1>fpar)) { par=par1;fpar=fpar1 }
-}
-if(control$REPORT>=1) cat("best:",par,"f:",fpar,"\n")
-return(list(sol=par,eval=fpar))
+hclimbing <- function(par,fn,change,lower,upper,control,
+                   type="min",...) {
+  fpar <- fn(par,...)
+  for(i in 1:control$maxit) { 
+    par1 <- change(par,lower,upper) 
+    fpar1 <- fn(par1,...)
+    if(control$REPORT>0 &&(i==1||i%%control$REPORT==0)) 
+      cat("i:",i,"s:",par,"f:",fpar,"s'",par1,"f:",fpar1,"\n")
+    if((type=="min" && fpar1<fpar) 
+          || (type=="max" && fpar1>fpar)) { par=par1;fpar=fpar1 }
+  }
+  if(control$REPORT>=1) cat("best:",par,"f:",fpar,"\n")
+  return(list(sol=par,eval=fpar))
 }
 
 # slight random change of vector par:
@@ -35,11 +34,11 @@ return(list(sol=par,eval=fpar))
 #    round - use integer (TRUE) or continuous (FALSE) search
 #    ... - extra parameters for dist
 #    examples: dist=rnorm, mean=0, sd=1; dist=runif, min=0,max=1
-hchange=function(par,lower,upper,dist,round=TRUE,...)
-{ D=length(par) # dimension
-step=dist(D,...) # slight step
-if(round) step=round(step) 
-par1=par+step
-# return par1 within [lower,upper]:
-return(ifelse(par1<lower,lower,ifelse(par1>upper,upper,par1)))
+hchange <- function(par,lower,upper,dist,round=TRUE,...) {
+  D <- length(par) # dimension
+  step <- dist(D,...) # slight step
+  if(round) step <- round(step) 
+  par1 <- par+step
+  # return par1 within [lower,upper]:
+  return(ifelse(par1<lower,lower,ifelse(par1>upper,upper,par1)))
 }
